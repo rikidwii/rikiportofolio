6325000023a30b5e90a937bacab422985920fa0f
rikidwii <rikidwiardi@gmail.com>
2025-11-17 18:18:52 +0700

    fix: rename navbar and footer casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Navbar from "./components/navbar";
-import Footer from "./components/footer";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 import Home from "./components/Home";
 import About from "./components/About";
 import Project from "./components/Project";
